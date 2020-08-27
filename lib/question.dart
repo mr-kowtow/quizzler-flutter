a5a828ea5f0e36f884010dda93e7c384b285ac0f
Rosh Boi <jhagroshan17@gmail.com>
2020-08-28 00:37:24 +1000

    added syntactic sugar example

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,11 @@ class Question {
   String questionText;
   bool questionAnswer;
   
-  Question(String q, bool a) {
-    questionText = q;
-    questionAnswer = a;
-  }
+//  Question(String q, bool a) {
+//    questionText = q;
+//    questionAnswer = a;
+//  }
+  
+  //syntactic sugar, below code equal line 5->8
+  Question(this.questionText, this.questionAnswer);
 }
